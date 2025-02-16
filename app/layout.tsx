34d176980b1f90d0d41fbc0ca1e0e940b3c3fe54
thekevinrex <39094357+thekevinrex@users.noreply.github.com>
2025-02-16 01:28:22 -0500

    [FEATURE] Add pagination and select components for improved UI functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 			<body className={`${fontSans.className} antialiased`}>
 				{children}
 
-				<Toaster />
+				<Toaster position="top-center" />
 			</body>
 		</html>
 	);
